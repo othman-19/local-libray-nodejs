ffa5c3d0e00833a70f0130169675ee4d9666aef9
othmane <othman-19@hotmail.fr>
2020-09-13 13:01:22 +0100

    Add catalog routes to the midleware chain

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+// Import routes for "catalog" area of site
+const catalogRouter = require('./routes/catalog');
 
 const app = express();
 
@@ -31,6 +33,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// Add catalog routes to middleware chain.
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
