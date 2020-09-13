7d7227db0b828d377ed77cbd60826fa37e9d4651
othmane <othman-19@hotmail.fr>
2020-09-13 11:54:28 +0100

    add the catalogue route module

---
+++ routes/catalog.js
@@ -0,0 +1,10 @@
+/* eslint-disable camelcase */
+const express = require('express');
+
+const router = express.Router();
+
+// Require controller modules.
+const book_controller = require('../controllers/bookController');
+const author_controller = require('../controllers/authorController');
+const genre_controller = require('../controllers/genreController');
+const book_instance_controller = require('../controllers/bookinstanceController');
