438e3e24f69d14f4cdb1734443927ec83823bcd5
othmane <othman-19@hotmail.fr>
2020-09-13 12:13:20 +0100

    add BookInstance routes

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,3 +1,4 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable camelcase */
 const express = require('express');
 
@@ -89,3 +90,49 @@ router.get('/genre/:id', genre_controller.genre_detail);
 
 // GET request for list of all Genre.
 router.get('/genres', genre_controller.genre_list);
+
+/// BOOKINSTANCE ROUTES ///
+
+// GET request for creating a BookInstance.
+// NOTE This must come before route that displays BookInstance (uses id).
+
+router.get(
+  '/bookinstance/create',
+  book_instance_controller.bookinstance_create_get
+);
+
+// POST request for creating BookInstance.
+router.post(
+  '/bookinstance/create',
+  book_instance_controller.bookinstance_create_post
+);
+
+// GET request to delete BookInstance.
+router.get(
+  '/bookinstance/:id/delete',
+  book_instance_controller.bookinstance_delete_get
+);
+
+// POST request to delete BookInstance.
+router.post(
+  '/bookinstance/:id/delete',
+  book_instance_controller.bookinstance_delete_post
+);
+
+// GET request to update BookInstance.
+router.get(
+  '/bookinstance/:id/update',
+  book_instance_controller.bookinstance_update_get
+);
+
+// POST request to update BookInstance.
+router.post(
+  '/bookinstance/:id/update',
+  book_instance_controller.bookinstance_update_post
+);
+
+// GET request for one BookInstance.
+router.get('/bookinstance/:id', book_instance_controller.bookinstance_detail);
+
+// GET request for list of all BookInstance.
+router.get('/bookinstances', book_instance_controller.bookinstance_list);
