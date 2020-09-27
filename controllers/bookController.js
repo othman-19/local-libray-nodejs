2d6d7b77890a4ef7a8bf3dcb1fa264ec4c8dd907
othmane <othman-19@hotmail.fr>
2020-09-27 17:27:08 +0100

    add the book_create_get function to the authors controller

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -85,8 +85,28 @@ exports.book_detail = function (req, res, next) {
 };
 
 // Display book create form on GET.
-exports.book_create_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Book create GET');
+exports.book_create_get = function (req, res, next) {
+  // Get all authors and genres, which we can use for adding to our book.
+  async.parallel(
+    {
+      authors(callback) {
+        Author.find(callback);
+      },
+      genres(callback) {
+        Genre.find(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      res.render('book_form', {
+        title: 'Create Book',
+        authors: results.authors,
+        genres: results.genres,
+      });
+    },
+  );
 };
 
 // Handle book create on POST.
