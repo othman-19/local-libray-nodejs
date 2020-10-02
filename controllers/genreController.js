8a0db491dd84ec5a7d1871d62098a6f04dad1b6f
othmane <othman-19@hotmail.fr>
2020-10-02 17:45:42 +0100

    add the genre_update_get method to genres controller

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -172,8 +172,17 @@ exports.genre_delete_post = function (req, res, next) {
 };
 
 // Display Genre update form on GET.
-exports.genre_update_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre update GET');
+exports.genre_update_get = function (req, res, next) {
+  Genre.findById(req.params.id, (err, genre) => {
+    if (err) {
+      return next(err);
+    }
+    if (genre == null) {
+      res.redirect('/catalog/genres');
+    } else {
+      res.render('genre_form', { title: 'Update Genre', genre });
+    }
+  }).exec();
 };
 
 // Handle Genre update on POST.
