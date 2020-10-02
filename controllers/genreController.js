5c58ffa8465629327252a20c6953c446f813c8f3
othmane <othman-19@hotmail.fr>
2020-10-02 18:03:57 +0100

    add the genre_update_post method to genres controller

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -186,6 +186,53 @@ exports.genre_update_get = function (req, res, next) {
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre update POST');
-};
+exports.genre_update_post = [
+  // Validate that the name field is not empty.
+  validator.body('name', 'Genre name required').trim().isLength({ min: 1 }),
+
+  // Sanitize (escape) the name field.
+  validator.sanitizeBody('name').escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validator.validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    const genre = new Genre({ name: req.body.name, _id: req.params.id });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render('genre_form', {
+        title: 'Update Genre',
+        genre,
+        errors: errors.array(),
+      });
+    } else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      Genre.findOne({ name: req.body.name }).exec((err, found_genre) => {
+        if (err) {
+          return next(err);
+        }
+        if (found_genre) {
+          // Genre exists, redirect to its detail page.
+          res.redirect(found_genre.url);
+        } else {
+          Genre.findByIdAndUpdate(
+            req.params.id,
+            genre,
+            {},
+            (error, updated_genre) => {
+              if (error) {
+                return next(error);
+              }
+              // Successful - redirect to book detail page.
+              res.redirect(updated_genre.url);
+            },
+          );
+        }
+      });
+    }
+  },
+];
