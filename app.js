0c7ae04a90bdbd882eb2b982c523a3d954c19c44
othmane <othman-19@hotmail.fr>
2020-10-04 18:11:37 +0100

    Require the compression library then add it to the midleware chain

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const compression = require('compression');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -30,6 +31,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(compression()); // Compress all routes
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
